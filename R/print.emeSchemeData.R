94dff0a2a4b79bc0cb519613a1813cc1f60381b0
Rainer M. Krug <Rainer@krugs.de>
2019-01-09 10:57:54 +0100

    Add generic functions for emeScheme

---
+++ R/print.emeSchemeData.R
@@ -0,0 +1,36 @@
+#' Generic print function for \code{emeSchemeData}
+#'
+#' @param x object of type \code{emeSchemeSet}
+#' @param printAttr default \code{TRUE} - attributes are printed
+#' @param printExtAttr default \code{FALSE} - additional attributes are not printed (e.g. \code{class})
+#' @param printData default \code{TRUE} - data is printed
+#' @param .prefix mainly for internal use - prefix for all printed lines
+#'
+#' @return invisibly x
+#' @export
+#'
+print.emeSchemeData <- function(x, printAttr = TRUE, printExtAttr = FALSE, printData = TRUE, .prefix = ""){
+  ##
+  cat_ln(.prefix, " ", attr(x, "propertyName"), " - emeSchemeData")
+  if (printAttr) {
+    cat_ln("A   ", .prefix, "Names : ", paste(attr(x, "names"), collapse = " "))
+    cat_ln("A   ", .prefix, "Units : ", paste(attr(x, "unit"), collapse = " "))
+    cat_ln("A   ", .prefix, "Class : ", paste(attr(x, "type"), collapse = " "))
+    cat_ln()
+  }
+  #
+  if (printExtAttr) {
+    cat_ln("XA  ", .prefix, "Types : ", paste(attr(x, "class"), collapse = " "))
+    cat_ln()
+  }
+  ##
+  if (printData) {
+    x <- as.data.frame(x)
+    .prefix <- paste0("D   ", .prefix, row.names(x))
+    .rowNamesDF(x, make.names = TRUE) <- .prefix
+    print(x)
+    cat_ln()
+  }
+  ##
+  invisible(x)
+}
