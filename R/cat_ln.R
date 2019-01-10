ce11d307061b1b4a6761a7137f1587d3e3c2dde0
Rainer M. Krug <Rainer@krugs.de>
2019-01-10 10:07:54 +0100

    cat_ln.R: Fix documentation

--- R/cat_ln.R
+++ R/cat_ln.R
@@ -1,5 +1,7 @@
 #' cat with linefeed at the end Title
 #'
+#' Copied from tibble::cat_line()
+#' @param ... will be handed over to \code{cat(..., "\n")}
 cat_ln <- function(...){
   cat(paste0( ...,  "\n"), sep = "")
 }
