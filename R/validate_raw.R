fe70740e307720b9f1810c6cc2d669c51af564c3
Rainer M. Krug <Rainer@krugs.de>
2019-02-05 11:24:30 +0100

    Update emeScheme and validation
    * some changes in the structure
    * added validation

--- R/validate_raw.R
+++ R/validate_raw.R
@@ -1,9 +1,13 @@
 #' Validate structure of object of class \code{emeSchemeSet_raw} against \code{emeScheme}.
 #'
 #' @param x object of class \code{emeSchemeSet_raw} as returned from \code{read_from_excel( keepData = FALSE, raw = TRUE)}
-#' @param errorIfFalse if \code{TRUE} an error will be raised if the schemes are nbot identical
+#' @param errorIfFalse if \code{TRUE} an error will be raised if the schemes are not identical, i.e. there are structural differences.
 #'
 #' @return \code{TRUE} if they are identical or character vector describing the differences
+#'
+#' @importFrom taxize gnr_resolve
+#' @importFrom magrittr %>% %<>%
+#' @importFrom dplyr filter
 #' @export
 #'
 #' @examples
@@ -13,10 +17,100 @@ validate_raw <- function(
   x,
   errorIfFalse = TRUE
   ) {
-  result <- all.equal(new_emeSchemeSet( x, keepData = FALSE, verbose = FALSE), emeScheme)
-  if (!isTRUE(result) & errorIfFalse){
+
+# HELPER: Validate Species ------------------------------------------------
+
+validateSpecies <- function( x ){
+  result <- list()
+
+  # Validate species names --------------------------------------------------
+  result$speciesNames <- taxize::gnr_resolve(x$name, best_match_only = TRUE)
+  return(result)
+}
+
+# HELPER: Validate Columns ------------------------------------------------
+
+valCol <- function( x ){
+  cols <- unique(x$variableName)
+  names(cols) <- cols
+
+  result$columns <- lapply(
+    cols,
+    function (col) {
+      result <- list()
+      result$correctType <- FALSE
+      result$inDataFile <- FALSE
+      result$largerMin <- FALSE
+      result$smallerMax <- FALSE
+      result$inAllowedValues <- FALSE
+    }
+  )
+  return(result)
+}
+
+
+# HELPER: Validate DataFileMetaData ---------------------------------------
+
+validateDataFileMetaData <- function( x ){
+  dfns <- unique(x$dataFileName)
+  names(dfns) <- dfns
+  result <- lapply(
+    dfns,
+    function(dfn) {
+      result <- list()
+
+      # Validate existence of data files ----------------------------------------
+
+      result$fileExists <- file.exists(dfn)
+      if (file.exists(dfn)) {
+
+        # Validate columne --------------------------------------------------------
+
+        result$valColumns <- valCol(
+          x %>% dplyr::filter(.data$dataFileName == dfn)
+        )
+
+      }
+      return(result)
+    }
+  )
+  return(result)
+}
+
+
+# Define result structure -------------------------------------------------
+
+  result <- list()
+
+# Validate structure ------------------------------------------------------
+
+  struct <- new_emeSchemeSet( x, keepData = FALSE, verbose = FALSE)
+  result$structOK <- isTRUE(all.equal(struct, emeScheme))
+  if (!result$structOK & errorIfFalse){
     cat_ln(result)
     stop("x would result in a scheme which is not identical to the emeScheme!")
   }
+
+# Validata data -----------------------------------------------------------
+
+
+  if (result$structOK){
+    dat <- new_emeSchemeSet( x, keepData = TRUE, verbose = FALSE)
+
+  # Validate Species --------------------------------------------------------
+
+    result$Species <- validateSpecies(dat$Species)
+
+  # Validate DataFileMetaData -----------------------------------------------
+
+    result$DataFileMetaData <- validateDataFileMetaData(dat$DataFileMetaData)
+
+
+  }
+
+# Return result -----------------------------------------------------------
+
   return(result)
 }
+
+
--- tests/testthat/test-validate_raw.R
+++ tests/testthat/test-validate_raw.R
@@ -4,38 +4,39 @@ context("validate_raw()")
 # returns TRUE -------------------------------------------------------------
 
 test_that(
-  "validata_raw() returns TRUE when correct",
+  "validata_raw() returns correct value when correct",
   {
-    expect_true(
-      object = validate_raw( x = emeScheme_raw, errorIfFalse = TRUE)
+    expect_known_value(
+      object = validate_raw( x = emeScheme_raw, errorIfFalse = TRUE),
+      file = "validate_raw.CORRECT.rda"
     )
   }
 )
 
 
-# Fails when not correct -----------------------------------------------
+# returns differences when nt correct -------------------------------------
 
 x <- emeScheme_raw
 names(x)[1] <- "experiment"
-test_that(
-  "validata_raw() fails",
-  {
-    expect_error(
-      object = capture.output( validate_raw( x = x, errorIfFalse = TRUE) ),
-      regexp = ("x would result in a scheme which is not identical to the emeScheme!")
-    )
-  }
-)
-
-# returns differences when nt correct -------------------------------------
 
 test_that(
   "validata_raw() fails",
   {
     expect_known_value(
-      object = validate_raw( x = x, errorIfFalse = FALSE),
-      file = "validate_raw.rda",
-      update = TRUE
+      object =  validate_raw( x = x, errorIfFalse = FALSE),
+      file = "validate_raw.DIFFERENCES.rda"
+    )
+  }
+)
+
+# Fails when not correct -----------------------------------------------
+
+test_that(
+  "validata_raw() fails",
+  {
+    expect_error(
+      object = validate_raw( x = x, errorIfFalse = TRUE),
+      regexp = ("x would result in a scheme which is not identical to the emeScheme!")
     )
   }
 )
