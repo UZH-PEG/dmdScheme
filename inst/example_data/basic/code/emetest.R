5216e78b5dce4d6859a7515a8d154cf1b1faea03
Rainer M. Krug <Rainer@krugs.de>
2019-02-11 17:28:50 +0100

    add emetest.R to example

---
+++ inst/example_data/basic/code/emetest.R
@@ -0,0 +1,25 @@
+# install the package
+# only run once
+#devtools::install_github("Exp-Micro-Ecol-Hub/emeScheme", build_opts = c("--no-resave-data"))
+
+# load the library
+library(emeScheme)
+
+# open the user manual
+vignette("user_manual", "emeScheme")
+#browseVignettes("emeScheme")
+
+# open a clean metadata spreadsheet in xl
+# only run once
+#enter_new_metadata()
+
+# open a metadata spreadsheet in xl, containing the basic example entries
+# only run once
+#enter_new_metadata(keepData = TRUE)
+
+# create a folder in the working directory with the basic example project
+# only run once
+#make_example("basic")
+
+# validate the basic example
+validate("emeScheme.xlsx", path="data/archiving_data", report = "html")
