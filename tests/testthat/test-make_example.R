a05ce7deb5746261dd5f942051da0e0dac643f31
Rainer M. Krug <Rainer@krugs.de>
2019-02-11 13:57:32 +0100

    test-make_example: add tests

---
+++ tests/testthat/test-make_example.R
@@ -0,0 +1,32 @@
+context("make_example()")
+
+
+test_that(
+  "make_example returns list of examples when called without argument",
+  {
+    expect_known_output(
+      object = make_example(),
+      file = "make_example_.txt",
+    )
+  }
+)
+
+test_that(
+  "make_example() raises error when called with non-existing name",
+  {
+    expect_error(
+      object = make_example(name = "DOES_NOT+EXIST"),
+      regexp = "Invalid example. 'name' has to be one of the following values:"
+    )
+  }
+)
+
+setwd(tempdir())
+test_that(
+  "Does the copying without errors",
+  {
+    expect_null(
+      object = make_example("basic")
+    )
+  }
+)
