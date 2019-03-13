961f4c82310c1cd8f2e0d32a774180e5db67e25e
Rainer Krug <Rainer@krugs.de>
2019-03-13 15:19:13 +0100

    tests/testthat/test-emeScheme_split.R: Change to fix fail on travis

--- tests/testthat/test-emeScheme_split.R
+++ tests/testthat/test-emeScheme_split.R
@@ -32,11 +32,23 @@ test_that(
 test_that(
   "emeScheme_split( ) saves rds correctly",
   {
-    x <- emeScheme_split( emeScheme_example, c("rds", "xml"), path = tempdir() )
-    names(x) <- gsub(tempdir(), ".", x)
-    expect_equivalent(
-      object = md5sum(x), ## the generated ones
-      expected = md5sum(names(x)) # the reference ones stored in the tests directory
+    x <- emeScheme_split( emeScheme_example, "rds", path = tempdir() )
+    ref <- gsub(tempdir(), ".", x)
+    expect_equal(
+      object = lapply(x, readRDS), ## the generated ones
+      expected = lapply(ref, readRDS) # the reference ones stored in the tests directory
+    )
+  }
+)
+
+test_that(
+  "emeScheme_split( ) saves xml correctly",
+  {
+    x <- emeScheme_split( emeScheme_example, "xml", path = tempdir() )
+    ref <- gsub(tempdir(), ".", x)
+    expect_equal(
+      object = lapply(x, readLines), ## the generated ones
+      expected = lapply(ref, readLines) # the reference ones stored in the tests directory
     )
   }
 )
