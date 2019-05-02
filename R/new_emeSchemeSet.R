9f324b9330897b0519dcda734cbbb2ec0d054d19
Rainer M. Krug <Rainer@krugs.de>
2019-05-02 17:16:13 +0200

    new_emeScheme: remove import from tibble

--- R/new_emeSchemeSet.R
+++ R/new_emeSchemeSet.R
@@ -9,7 +9,6 @@
 #'
 #' @return \code{list} of \code{list} of ... \code{tibbles}
 #' @export
-#' @importFrom tibble is.tibble as_tibble
 #' @importFrom dplyr select starts_with
 #' @importFrom methods is as
 #' @importFrom magrittr set_names %<>%
