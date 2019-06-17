2092b9f299dd1f17ff58fa20cb20f4e64a1f3936
Rainer Krug <Rainer@krugs.de>
2019-06-17 10:00:33 +0200

    ./R/pkg.dmdScheme.R: Documentation

--- R/pkg.dmdScheme.R
+++ R/pkg.dmdScheme.R
@@ -1,19 +1,39 @@
-#' dmdScheme: A package containing the definition for a metadata scheme for Experimental Microcosm Ecological experiments.
+#' dmdScheme: A package containing the framework for Domain specific MetaData Schemes
 #'
-#' Metadata is essential for the managing of data. Consequentally, metadata
-#' schemes play an importent role in this. Nevertheless (or because of this),
+#' Metadata is essential for the managing and archiving of data. Consequentally, metadata
+#' schemes9, which standardise the property names used in specifying the metadata, play an essential role in this. Nevertheless (or because of this),
 #' metadata schemes are usuallu big, complex, difficult to read and understant,
-#' and, in consequence, are not used as often as they dshould be. Therefore, we
-#' developed a metadata scheme, called \code{dmdScheme} which is focussed on one
-#' homogeneous field, so that the metadata scheme is not to complex and can easiliy
-#' used.
-#' This package provides:
-#' 1) the authorative definition of the dmdScheme
-#' 2) object definitions for this scheme for R
-#' 3) Excel spreadsheet for entering the metadata for an experiment, i.e. multiple data files
-#' 4) functions to validate this metadata
-#' 5) functions to export the metadata to xml files, one per data file
+#' and, in consequence, are not used as often as they should be.
 #'
+#' This package provides a framework called `dmdScheme`, which
+#'
+#'   * **makes it easier develop a domain pecific metadata scheme**: by using a spreadsheet as the base for defining the mnew scheme
+#'   * **provides basic functionality for the new metadata scheme**: including entering, validationg, exporting and saving of the new metadata
+#'   * **makies it easier to enter new metadata**: by uising a spreadsheet to enter the new metadata which can than be imported and exported as xml
+#'
+#'
+#' This package provides for the **creator** of a new Domain Specific MetaData Scheme:
+#'   1. a simple way of discussing and developing a new scheme as it is
+#'   represented in a spreadsheet
+#'   2. a function to create a new R package for a
+#'   new domain specific metadata scheme which is based on a spreadsheet
+#'   containing the definition and inherits all the functionality of this
+#'   package (validation, printing, export, import, ...)
+#'   3. easy update of the new scheme based by re-importing the new
+#'   version of the scheme from the spreadsheet by si mply calling one function
+#'   4. easy extension of the functionality as the whole architecture is based
+#'   on S3 methods and the new scheme inherits from the `dmdScheme` objects.
+#'
+#'
+#' This package provides for the **user** of a Domain Specific MetaData Scheme:
+#'   1. the authorative definition of an example `dmdScheme`
+#'   2. object definitions for this scheme for R
+#'   3. Excel spreadsheet for entering the metadata for an experiment
+#'   4. functions to validate this metadata
+#'   5. functions to export the metadata to xml files, one per data file
+#'
+#'
+#' @md
 #' @docType package
 #' @name pkg.dmdScheme
 NULL
