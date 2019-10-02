b5e41dd30ecef31b105a9c0261a9932fa50a6cad
Rainer M. Krug <Rainer@krugs.de>
2019-10-02 15:48:26 +0200

    Intermediate second step

--- R/dmdScheme_to_xml.dmdSchemeData.R
+++ R/dmdScheme_to_xml.dmdSchemeData.R
@@ -1,11 +1,11 @@
 #' @export
 #'
-#' @importFrom XML xmlNode xmlAttrs append.xmlNode saveXML
+#' @importFrom xml2 xml_new_root xml_add_child xml_attrs write_xml
 #' @importFrom tibble is_tibble
 #'
 dmdScheme_to_xml.dmdSchemeData <- function(
   x,
-  file,
+  file = NULL,
   output = "metadata"
 ) {
 
@@ -16,58 +16,40 @@ dmdScheme_to_xml.dmdSchemeData <- function(
 
   # x is of type dmdSchemeData and therefore a tibble ------------------
 
-  xml <- xml2::xml_new_root("dmdSchemeData") %>%
-    xml2::xml_add_child(xml, paste0(attr(x, "propertyName"), "List"))
+  xml <- xml2::xml_new_root(paste0(attr(x, "propertyName"), "List"))
 
-  if (length(x) > 1) {
+  if (ncol(x) > 1) {
+    idName <- grep("ID", names(x)[[1]], value = TRUE)
     if (nrow(x) > 0) {
       for (i in 1:nrow(x)) {
-        xmlFields <- lapply(
-          names(x),
-          function(nm){
-            XML::xmlNode(
-              nm,
-              ifelse(is.na(x[[nm]][i]), "", x[[nm]][i])
-            )
-          }
-        )
-        id <- grep("ID", names(x))
-        if (length(id) > 0) {
-          if (id[[1]] == 1) {
-            id <- names(x)[1]
-            idi <- x[[1]][i]
-            names(idi) <- id
-            xmlField <- XML::xmlNode(name = attr(x, "propertyName"), attrs = idi)
-          }
-        } else {
-          xmlField <- XML::xmlNode(attr(x, "propertyName"))
+        xmlCol <- xml2::xml_new_root(attr(x, "propertyName"))
+        ##
+        if (length(idName) > 0) {
+          id <- x[[idName]][i]
+          names(id) <- idName
+          xml2::xml_attrs(xmlCol) <- id
         }
-        xmlField <- XML::append.xmlNode(
-          to = xmlField,
-          xmlFields
-        )
-        xml <- XML::append.xmlNode(
-          to = xml,
-          xmlField
-        )
+        ##
+        for (nm in names(x)) {
+          value <- ifelse(is.na(x[[nm]][i]), "", x[[nm]][i])
+          xml2::xml_add_child(xmlCol, nm, value)
+        }
+        xml2::xml_add_child(xml, xmlCol)
+        rm(xmlCol)
       }
     }
-
-  } else {
-    xml <- XML::xmlNode(attr(x, "propertyName"), x)
   }
 
   # Add attributes if output == complete ------------------------------------
 
   if (output == "complete") {
-    XML::xmlAttrs(
-      node = xml,
-      append = TRUE
-    ) <- sapply(
+    atrs <- sapply(
       attributes(x),
       paste,
-      collapse = " #&# "
+      collapse = " #%# "
     )
+    atrs <- atrs[-which(names(atrs) == "row.names")]
+    xml2::xml_attrs( xml ) <- atrs
   }
 
   # If file not NULL, i.e. from root node as file not used in it --------
