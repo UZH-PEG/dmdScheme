d3c96b64d958f6bda62fce1d53bee2ee57bf57ce
Rainer Krug <Rainer@krugs.de>
2019-11-07 11:27:08 +0100

    test-13-upgrade_old_files(): fix tests

--- tests/testthat/test-13-upgrade_old_files.R
+++ tests/testthat/test-13-upgrade_old_files.R
@@ -11,25 +11,17 @@ test_that(
   }
 )
 
-test_that(
-  "upgrade_old_files() raises error if file is of wrong scheme",
-  {
-    expect_error(
-      object = upgrade_old_files(file = system.file("emeScheme.xlsx", package = "dmdScheme")),
-      regexp = "The scheme is in a not in a loaded scheme definition."
-    )
-  }
-)
+
 
 test_that(
   "upgrade_old_files() gives warning and returns `NULL` if same version as current",
   {
     expect_warning(
-      object = upgrade_old_files(file = system.file("dmdScheme.xlsx", package = "dmdScheme")),
+      object = upgrade_old_files(file = system.file("installedSchemes", paste0(scheme_default(), ".xlsx"), package = "dmdScheme")),
       regexp = "File has same version as the installed package. No conversion necessary!"
     )
     expect_null(
-      object = suppressWarnings(upgrade_old_files(file = system.file("dmdScheme.xlsx", package = "dmdScheme")))
+      object = suppressWarnings(upgrade_old_files(file = system.file("installedSchemes", paste0(scheme_default(), ".xlsx"), package = "dmdScheme")))
     )
   }
 )
