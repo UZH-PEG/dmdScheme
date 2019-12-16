9123666c023cf4a0b4a0ab28ab8656f6f60b510b
Rainer M. Krug <Rainer@krugs.de>
2019-12-16 18:08:18 +0100

    Remove .skipOpenURL argument

--- tests/testthat/test-06-open_new_spreadsheet.R
+++ tests/testthat/test-06-open_new_spreadsheet.R
@@ -8,7 +8,7 @@ test_that(
   "open_new_spreadsheet() returns path after saving to file and opening - `format = TRUE`",
   {
     expect_equal(
-      object = suppressWarnings(open_new_spreadsheet(file = fn, format = TRUE, .skipBrowseURL = TRUE)),
+      object = suppressWarnings(open_new_spreadsheet(file = fn, format = TRUE, open = FALSE)),
       expected = fn,
     )
   }
@@ -18,7 +18,7 @@ test_that(
   "open_new_spreadsheet() reports error when file exist",
   {
     expect_error(
-      object = suppressWarnings(open_new_spreadsheet(file = fn, format = FALSE, .skipBrowseURL = TRUE)),
+      object = suppressWarnings(open_new_spreadsheet(file = fn, format = FALSE, open = FALSE)),
       regexp = "Error during copying of the file from"
     )
   }
@@ -28,7 +28,7 @@ test_that(
   "open_new_spreadsheet() returns path when file exist and overwrite = TRUE",
   {
     expect_equal(
-      object = suppressWarnings(open_new_spreadsheet(file = fn, format = FALSE, overwrite = TRUE, .skipBrowseURL = TRUE)),
+      object = suppressWarnings(open_new_spreadsheet(file = fn, format = FALSE, overwrite = TRUE, open = FALSE)),
       expected = fn,
     )
   }
@@ -40,7 +40,7 @@ test_that(
   "open_new_spreadsheet() creates file",
   {
     expect_true(
-      object = file.exists( suppressWarnings(open_new_spreadsheet(file = NULL, format = FALSE, open = TRUE, overwrite = FALSE, .skipBrowseURL = TRUE) ) )
+      object = file.exists( suppressWarnings(open_new_spreadsheet(file = NULL, format = FALSE, open = FALSE, overwrite = FALSE) ) )
     )
   }
 )
